00b050321da41f059fc86ac9327667bab894df40
tenmugi <89638729+tenmugi@users.noreply.github.com>
2021-10-01 09:19:47 +0900

    走らせる

--- Assets/unity-chan!/Unity-chan! Model/Art/Models/UnityChanController.cs
+++ Assets/unity-chan!/Unity-chan! Model/Art/Models/UnityChanController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.myAnimator = GetComponent<Animator>();
+
+        this.myAnimator.SetFloat("Speed", 1);
     }
 
     // Update is called once per frame
